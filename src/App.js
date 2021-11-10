ae291bcaa16122a6e204ba47dc93153d16487359
Santiago Cano <canosantiago404@gmail.com>
2021-11-10 15:12:03 -0300

    added App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import allLevelData from "./allLevelData";
+// components
+import Navbar from "./components/Navbar";
+// pages
+import Leaderboard from "./pages/Leaderboard";
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+import Info from "./pages/Info";
+// contexts
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
+  const [inGame, setInGame] = useState(false);
+  const [inLeaderboard, setInLeaderboard] = useState(false);
+  const [inHome, setInHome] = useState(false);
+  const [inInfo, setInInfo] = useState(false);
+  const [level, setLevel] = useState(0);
+  const [levelData, setLevelData] = useState(allLevelData);
+  const [characters, setCharacters] = useState([]);
+  const [username, setUsername] = useState("");
+  const handleUpdateUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router basename="/photo_tagging_app">
+      <Navbar
+        inGame={inGame}
+        setInGame={setInGame}
+        characters={characters}
+        inLeaderboard={inLeaderboard}
+        inHome={inHome}
+        inInfo={inInfo}
+      />
+      <div className="container">
+        <AuthProvider>
+          <Switch>
+            <Route exact path="/" component={Home}>
+              <Home
+                setLevel={setLevel}
+                levelData={levelData}
+                setInGame={setInGame}
+                setInLeaderboard={setInLeaderboard}
+                setInHome={setInHome}
+                setInInfo={setInInfo}
+              />
+            </Route>
+            <Route exact path="/game" component={Game}>
+              <Game
+                level={level}
+                username={username}
+                updateUsername={handleUpdateUsername}
+                levelData={levelData}
+                characters={characters}
+                setCharacters={setCharacters}
+                setInLeaderboard={setInLeaderboard}
+                setInHome={setInHome}
+                setInInfo={setInInfo}
+              />
+            </Route>
+            <Route path="/leaderboard">
+              <Leaderboard
+                setLevel={setLevel}
+                level={level}
+                levelData={levelData}
+                setInGame={setInGame}
+                setInLeaderboard={setInLeaderboard}
+                setInHome={setInHome}
+                setInInfo={setInInfo}
+              />
+            </Route>
+            <Route path="/info">
+              <Info
+                setInGame={setInGame}
+                setInLeaderboard={setInLeaderboard}
+                setInHome={setInHome}
+                setInInfo={setInInfo}
+              />
+            </Route>
+          </Switch>
+        </AuthProvider>
+      </div>
+    </Router>
   );
 }
 
